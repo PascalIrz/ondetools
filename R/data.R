ed530aa708739f808b7ca0d1efbe1b81f7344eb9
richaben <brichard.rb@gmail.com>
2023-05-30 16:53:58 +0200

    ajouts fonction rapport + data dpts FR

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Contours des départements français
+#'
+#' Un tableau contenant les informations géographiques (polygones) des départements
+#'  français. Les variables sont :
+#'
+#' @format un dataframe au 'sf' (multipolygone) crs RGF93
+#' \describe{
+#'   \item{code_insee}{le code insee des départements}
+#'   \item{nom}{le nom du département}
+#'   \item{geometry}{l'information sur la géométrie du département}
+#' }
+"departements_FR"
