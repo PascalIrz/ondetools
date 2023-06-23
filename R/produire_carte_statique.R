8488b138bed8472dfc957824feb3905d85de5d94
richaben <brichard.rb@gmail.com>
2023-06-23 17:27:45 +0200

    modif template + fonction produire_carte_statique

--- R/produire_carte_statique.R
+++ R/produire_carte_statique.R
@@ -4,63 +4,55 @@
 #' @param code_departement le code insee du département, par exemple '14' pour l'Eure
 #' @param referentiel_onde la typologie à utiliser,
 #'  soit \code{Typologie nationale} ou \code{Typologie départementale}
-#' @param couleurs un vecteur de couleur correspond aux référentiels
+#' @param couleurs un vecteur de couleurs correspondant aux référentiels
 #'  national ou départemental
 #' @param dptFR_shp une couche shape pour les départements FR
 #'
 #' @return un graphique \code{ggplot2}
 #' @export
 #'
-#' @importFrom dplyr filter recode
-#' @importFrom ggplot2 ggplot geom_sf scale_fill_manual coord_sf unit ggtitle theme_void theme element_text element_blank element_rect element_line
+#' @importFrom dplyr filter mutate case_when recode
+#' @importFrom ggplot2 ggplot geom_sf scale_fill_manual coord_sf unit ggtitle theme_void theme element_text element_blank element_rect element_line labs
 #' @importFrom ggspatial annotation_scale annotation_north_arrow
 #' @importFrom glue glue
 #' @importFrom lubridate month year
-#' @importFrom sf st_as_sf st_intersection st_geometry st_as_sfc st_bbox st_buffer
+#' @importFrom sf st_as_sf st_transform st_intersection st_geometry st_as_sfc st_bbox st_buffer
+#' @importFrom stringr str_wrap
 #'
 #' @examples
 #' \dontrun{
-#'
 #' dptFR <-
 #'   COGiter::departements_geo %>%
 #'   dplyr::left_join(COGiter::departements %>%
 #'                      dplyr::select(DEP, REG, NOM_DEP))
 #'
-#' produire_carte_statique(code_departement = '76',
-#'                         onde_df_mois = onde_df_sel,
-#'                         referentiel_onde = 'Typologie nationale',
-#'                         couleurs = onde_4mod,
-#'                         dptFR_shp = dptFR)
+#' produire_carte_statique(code_departement = '27',
+#'                          onde_df_mois = onde_df_mois2,
+#'                          referentiel_onde = 'Typologie départementale',
+#'                          couleurs = onde_5mod,
+#'                          dptFR_shp = dptFR)
 #'
-#' produire_carte_statique(code_departement = '76',
-#'                         onde_df_mois = onde_df_sel,
-#'                         referentiel_onde = 'Typologie départementale',
-#'                         couleurs = onde_5mod,
-#'                         dptFR_shp = dptFR)
+#' produire_carte_statique(code_departement = '27',
+#'                          onde_df_mois = onde_df_mois2,
+#'                          referentiel_onde = 'Typologie nationale',
+#'                          couleurs = onde_4mod,
+#'                          dptFR_shp = dptFR)
 #' }
-
 produire_carte_statique <- function(onde_df_mois = NULL,
                                     code_departement = NULL,
                                     referentiel_onde = 'Typologie nationale',
                                     couleurs = onde_4mod,
                                     dptFR_shp = NULL) {
-
   if (is.null(code_departement)) {
-    stop(
-      "code dpt manquant"
-    )
+    stop("code dpt manquant")
   }
 
   if (is.null(onde_df_mois)) {
-    stop(
-      "dataframe ONDE manquant"
-    )
+    stop("dataframe ONDE manquant")
   }
 
   if (is.null(dptFR_shp)) {
-    stop(
-      "shape FR dpt manquant"
-    )
+    stop("shape FR dpt manquant")
   }
 
   if (is.null(referentiel_onde) ||
@@ -72,7 +64,11 @@ produire_carte_statique <- function(onde_df_mois = NULL,
   }
 
   dpt_shp <-
-    {{dptFR_shp}} %>%
+    {
+      {
+        dptFR_shp
+      }
+    } %>%
     dplyr::filter(DEP == {
       {
         code_departement
@@ -81,27 +77,27 @@ produire_carte_statique <- function(onde_df_mois = NULL,
 
   onde_df_mois <-
     onde_df_mois %>%
-    dplyr::filter(CdDepartement == {
+    dplyr::filter(code_departement == {
       {
         code_departement
       }
     }) %>%
-    sf::st_as_sf(
-      coords = c('CoordXSiteHydro', 'CoordYSiteHydro'),
-      crs = unique(onde_df_mois$ProjCoordSiteHydro)
-    ) %>%
-    mutate(
-      LbRsObservationNat = sub(' ', '\n', LbRsObservationNat),
-      LbRsObservationDpt = ifelse(
-        LbRsObservationDpt == 'Ecoulement visible',
-        'Ecoulement visible acceptable',
-        LbRsObservationDpt
+    sf::st_as_sf(coords = c("longitude", "latitude"),
+                 crs = 4326) %>%
+    sf::st_transform(crs = 2154) %>% dplyr::mutate(
+      lib_ecoul3mod = dplyr::case_when(
+        libelle_ecoulement == 'Ecoulement visible faible' ~ 'Ecoulement visible',
+        libelle_ecoulement == 'Ecoulement visible acceptable' ~ 'Ecoulement visible',
+        TRUE ~ libelle_ecoulement
       ),
-      LbRsObservationDpt = ifelse(
-        LbRsObservationDpt == "Ecoulement non visible",
-        "Ecoulement\nnon visible",
-        gsub(" ", "\n", LbRsObservationDpt)
+      lib_ecoul4mod = dplyr::case_when(
+        libelle_ecoulement == 'Ecoulement visible' ~ 'Ecoulement visible acceptable',
+        TRUE ~ libelle_ecoulement
       )
+    ) %>%
+    dplyr::mutate(
+      Couleur_3mod = dplyr::recode(stringr::str_wrap(lib_ecoul3mod, 12),!!!onde_4mod),
+      Couleur_4mod = dplyr::recode(stringr::str_wrap(lib_ecoul4mod, 12),!!!onde_5mod)
     ) %>%
     {
       if ({
@@ -109,17 +105,13 @@ produire_carte_statique <- function(onde_df_mois = NULL,
           referentiel_onde
         }
       } == 'Typologie nationale')
-        mutate(
-          .,
-          observations = LbRsObservationNat,
-          couleurs = dplyr::recode(LbRsObservationNat,!!!onde_4mod)
-        )
+        mutate(.,
+               observations = stringr::str_wrap(lib_ecoul3mod, 12),
+               couleurs = Couleur_3mod)
       else
-        mutate(
-          .,
-          observations = LbRsObservationDpt,
-          couleurs = dplyr::recode(LbRsObservationDpt,!!!onde_5mod)
-        )
+        mutate(.,
+               observations = stringr::str_wrap(lib_ecoul4mod, 12),
+               couleurs = Couleur_4mod)
     }
 
   ggplot2::ggplot() +
@@ -144,14 +136,12 @@ produire_carte_statique <- function(onde_df_mois = NULL,
       size = 3.5,
       alpha = 0.8
     ) +
-    ggplot2::scale_fill_manual(
-      name = glue::glue("Situation stations"),
-      values = {
-        {
-          couleurs
-        }
-      }
-    ) +
+    ggplot2::scale_fill_manual(name = glue::glue("Situation stations"),
+                               values = {
+                                 {
+                                   couleurs
+                                 }
+                               }) +
     ggplot2::coord_sf(
       xlim = sf::st_bbox(dpt_shp)[c(1, 3)],
       ylim = sf::st_bbox(dpt_shp)[c(2, 4)],
@@ -170,16 +160,20 @@ produire_carte_statique <- function(onde_df_mois = NULL,
     ) +
     ggplot2::ggtitle(
       label = glue::glue(
-        'R\u00e9seau ONDE - {unique(dpt_shp$NOM_DEP)} - {unique(lubridate::month(onde_df_mois$DtRealObservation,label = T, locale = \"fr_FR\"))} {unique(lubridate::year(onde_df_mois$DtRealObservation))} - {referentiel_onde}'
-      )
+        'R\u00e9seau ONDE - {unique(onde_df_mois$libelle_departement)} - Campagne {unique(onde_df_mois2$libelle_type_campagne)} {unique(lubridate::month(onde_df_mois$date_campagne,label = T, locale = \"fr_FR\"))} {unique(lubridate::year(onde_df_mois$date_campagne))}'
+      ),
+      subtitle = glue::glue('{referentiel_onde}')
     ) +
+    ggplot2::labs(caption = paste("Source: ONDE (OFB)\n ©OFB", format(Sys.time(), '%Y'), "- Date d'édition:", format(Sys.time(), '%d/%m/%Y'))) +
     ggplot2::theme_void() +
     ggplot2::theme(
-      legend.text = ggplot2::element_text(size = 8),
+      legend.text = ggplot2::element_text(size = 10),
       legend.title =  ggplot2::element_text(face = "bold",
-                                            size = 8),
+                                            size = 9),
       plot.title = ggplot2::element_text(face = "bold",
-                                         size = 8),
+                                         size = 10),
+      plot.subtitle = ggplot2::element_text(face = "italic",
+                                            size = 10),
       axis.text = ggplot2::element_blank(),
       axis.title = ggplot2::element_blank(),
       axis.ticks.x = ggplot2::element_blank(),
@@ -191,3 +185,4 @@ produire_carte_statique <- function(onde_df_mois = NULL,
       plot.background = ggplot2::element_blank()
     )
 }
+
